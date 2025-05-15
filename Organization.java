21a36da9e35d24044486614c01b7f45fa4d8aa90
Tesfaye <tesfayealemayehu796@gmail.com>
2025-05-15 15:40:55 +0300

    use case finished

--- Organization.java
+++ Organization.java
@@ -29,3 +29,29 @@ public class Organization {
         throw new UnsupportedOperationException();
     }
 }
+public class Property {
+
+    private String id;
+    private String title;
+    private String location;
+    private Float price;
+    private String description;
+    private String status;
+    private String phase;
+    private String community;
+
+    public void viewDetails() {
+        // TODO - implement viewDetails logic
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void edit() {
+        // TODO - implement edit logic
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void assignToGroup() {
+        // TODO - implement assignToGroup logic
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ orga.java
@@ -0,0 +1,58 @@
+public class Organization {
+
+          private String id;
+          private String name;
+          private String contactInfo;
+      
+          public void login() {
+              // TODO - implement organization.login
+              throw new UnsupportedOperationException();
+          }
+      
+          public void logout() {
+              // TODO - implement organization.logout
+              throw new UnsupportedOperationException();
+          }
+      
+          public void sendPropertyFile() {
+              // TODO - implement organization.sendPropertyFile
+              throw new UnsupportedOperationException();
+          }
+      
+          public void providePropertyAssets() {
+              // TODO - implement organization.providePropertyAssets
+              throw new UnsupportedOperationException();
+          }
+      
+          public void setListingPolicies() {
+              // TODO - implement organization.setListingpolicies
+              throw new UnsupportedOperationException();
+          }
+      }
+      public class Property {
+      
+          private String id;
+          private String title;
+          private String location;
+          private Float price;
+          private String description;
+          private String status;
+          private String phase;
+          private String community;
+      
+          public void viewDetails() {
+              // TODO - implement viewDetails logic
+              throw new UnsupportedOperationException("Not implemented yet");
+          }
+      
+          public void edit() {
+              // TODO - implement edit logic
+              throw new UnsupportedOperationException("Not implemented yet");
+          }
+      
+          public void assignToGroup() {
+              // TODO - implement assignToGroup logic
+              throw new UnsupportedOperationException("Not implemented yet");
+          }
+      }
+      
